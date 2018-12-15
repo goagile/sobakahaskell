015a01c782b3aab35be78d6ce3ed9e90d45ebd26
khardi <kharchilindanila@gmail.com>
2018-12-15 20:56:33 +0300

    ADD: list sum & prod

--- list/main.hs
+++ list/main.hs
@@ -11,6 +11,15 @@ main = do
     print "names: "
     print names
 
+    print "name #3: "
+    print (names !! 2)
+
+    print "take 4: "
+    print (take 4 names)
+
+    print "drop 2: "
+    print (drop 2 names)
+
     print "head: "
     print (head names)
     
@@ -22,3 +31,23 @@ main = do
     
     print "last: "
     print (last names)
+
+    print "length: "
+    print (length names)
+
+    print "id null: "
+    print (null names)
+
+    print "reverse: "
+    print (reverse names)
+
+    let nums = sort [2, 3, 5, 4, 1]
+
+    print "nums: "
+    print nums
+
+    print "sum: "
+    print (sum nums)
+
+    print "product: "
+    print (product nums)
