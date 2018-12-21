aa28867053d9d34c2d36bdb525b89e3161d5f0be
khardi <kharchilindanila@gmail.com>
2018-12-21 13:48:07 +0300

    max repl take

--- list/emulate/main.hs
+++ list/emulate/main.hs
@@ -19,11 +19,32 @@ main = do
     putStrLn ("filt >2 [1, 2, 3] = " ++ (show $ filt (\x -> x > 2) [1, 2, 3]))
     putStrLn ""
 
+    putStrLn "max':"
+    putStrLn ("max' 2 5 = " ++ show (max' 2 5))
+    putStrLn ""
+
+    putStrLn "maxim:"
+    putStrLn ("maxim [1, 2, 3] = " ++ show (maxim [1, 2, 3]))
+    putStrLn ""
+
+    putStrLn "repl:"
+    putStrLn ("repl 5 'A' = " ++ show (repl 5 'A'))
+    putStrLn ""
+
+    putStrLn "repl':"
+    putStrLn ("repl' 5 'A' = " ++ show (repl' 5 'A'))
+    putStrLn ""
+
+    putStrLn "take':"
+    putStrLn ("take' 2 [1, 2, 3] = " ++ show (take' 2 [1, 2, 3]))
+    putStrLn ""
+
 
 -- len
 len :: [a] -> Int
 len [] = 0
 len (_:xs) = 1 + len xs
+
 
 -- filt
 filt :: (a -> Bool) -> [a] -> [a]
@@ -32,7 +53,44 @@ filt f (x:xs) = if f x == True
                 then x:(filt f xs)
                 else filt f xs
 
+
 -- elm
 elm :: Eq a => a -> [a] -> Bool
 elm _ [] = False
-elm a n = 0 /= len (filter (\ z -> z == a) n)
+elm x xs = 0 /= len found
+    where found = filt ((==) x) xs
+
+
+-- max' 
+max' :: (Ord a) => a -> a -> a
+max' a b = if a > b 
+           then a
+           else b
+
+
+-- maxim
+maxim :: (Ord a) => [a] -> a
+maxim [] = error "list is empty"
+maxim [x] = x
+maxim (x:xs) = max x (maxim xs)
+
+
+-- repl
+repl :: Int -> a -> [a]
+repl 0 _ = []
+repl n x = x : repl (n-1) x
+
+
+-- repl'
+repl' :: Int -> a -> [a]
+repl' n x
+    | n <= 0 = []
+    | otherwise = x : repl' (n-1) x
+
+
+-- take'
+take' :: Int -> [b] -> [b]
+take' n _
+    | n <= 0 = []
+take' _ [] = []
+take' n (x:xs) = x : take' (n-1) xs
